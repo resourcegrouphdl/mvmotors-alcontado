a573275135d4a6b77dfeb8385f639aa70057cecc
daniel <daniel.lizana01@gmail.com>
2025-11-25 23:43:01 -0500

    asd

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {NgxSonnerToaster} from 'ngx-sonner';
-import {Footer} from './shared/footer/footer';
-import {Tablaclientes} from './shared/tablaclientes/tablaclientes';
-import {Navbar} from './shared/navbar/navbar';
-import {TopInformationBar} from './shared/top-information-bar/top-information-bar';
-import {LogosInMove} from './shared/logos-in-move/logos-in-move';
 import {Home} from './features/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgxSonnerToaster, Footer, Tablaclientes, Navbar, TopInformationBar, LogosInMove, Home],
+  imports: [ Home],
   templateUrl: './app.html',
   standalone: true,
   styleUrl: './app.css'
