e56e9272f1aa27aff9c12fd3e62a13b1cedb03ee
daniel <daniel.lizana01@gmail.com>
2025-11-25 23:36:18 -0500

    se ordeno un poco

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import {NotFound} from './shared/not-found/not-found';
 import {LibroDeReclamaciones} from './shared/libro-de-reclamaciones/libro-de-reclamaciones';
 import {PoliticasDePrivacidad} from './shared/politicas-de-privacidad/politicas-de-privacidad';
 import {TerminosCondiciones} from './shared/terminos-condiciones/terminos-condiciones';
-import {Inicio} from './inicio/inicio/inicio';
+import {Inicio} from './features/inicio/inicio';
 
 export const routes: Routes = [
   {
--- src/app/features/slider-card/slider-card.ts
+++ src/app/features/slider-card/slider-card.ts
@@ -1,6 +1,6 @@
 import {Component, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {StatusService} from '../../features/status/services/status.service';
+import {StatusService} from '../status/services/status.service';
 
 
 
